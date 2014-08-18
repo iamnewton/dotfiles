64357d70941bb8771a28d011c0371ce210d6c8b6
Newton <chrisohpedia@gmail.com>
2014-08-17 21:19:45 -0700

    Create library tests
    - Create simple tests currently for the cli arguments
    - Update README
    
    Issue: #8

---
+++ tests/cli.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env roundup
+
+dotfiles="./bin/dotfiles"
+
+describe "cli"
+
+it_shows_help() {
+	$dotfiles --help | grep "usage: dotfiles"
+}
+it_shows_a_version() {
+	$dotfiles --version | grep "Version"
+}
+it_prints_the_install_directory() {
+	status=$(set +e ; $dotfiles --prefix >/dev/null ; echo $?)
+	test 0 -eq $status
+}
+it_shows_usage_after_invalid_option() {
+	$dotfiles --asdf | grep "usage: dotfiles"
+}
